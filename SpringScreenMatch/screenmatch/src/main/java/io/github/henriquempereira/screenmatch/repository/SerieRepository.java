57668393fb5e295b976720f173230e8bc217d331
Henrique Morey Pereira <henriquemoreypereira@gmail.com>
2026-03-15 12:47:55 -0300

    feat: Adiciona busca por nome do ator e avaliação mínima. Corrige switch-case.

--- SpringScreenMatch/screenmatch/src/main/java/io/github/henriquempereira/screenmatch/repository/SerieRepository.java
+++ SpringScreenMatch/screenmatch/src/main/java/io/github/henriquempereira/screenmatch/repository/SerieRepository.java
@@ -3,8 +3,11 @@ package io.github.henriquempereira.screenmatch.repository;
 import io.github.henriquempereira.screenmatch.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SerieRepository extends JpaRepository <Serie, Long> {
     Optional<Serie> findByTitleContainingIgnoreCase(String serieName);
+
+    List<Serie> findByActorsContainingIgnoreCaseAndRatingGreaterThan(String actorName, double rating);
 }
